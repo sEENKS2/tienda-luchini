0e0589a570110d15d7afa6db2c05c9be7115eda8
sEENKS2 <leonel.luchini@gmail.com>
2022-06-02 18:28:07 -0300

    Se sincronizo counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
 
-const ItemCount = () => {
-    const [ count, setCount] = useState(0)
+const ItemCount = ({onConfirm, stock, initial = 1}) => {
+    const [ count, setCount] = useState(initial)
 
     const decrement = () => {
-        if (count > -5){
-            setCount((count) => count -1 )
-        }
+            setCount(count - 1)
     }
 
     const increment = () => {
-        if (count < 5)
-        setCount((count) => count + 1)
+        if (count < stock)
+        setCount(count + 1)
     }
 
     return(
-        <div style={{display: 'flex'}}>
-            <button onClick={decrement}> - </button>
-            <h1> {count} </h1>
-            <button onClick={increment}> + </button>
+        <div className="container asd">
+            <div className="container asd">
+                <button className="button" onClick={decrement}> - </button>
+                <h1> {count} </h1>
+                <button className="button" onClick={increment}> + </button>
+            </div>
+            <button className='button' onClick={() => onConfirm(count)}>Agregar al carrito</button>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,17 @@
 import '../../item.css'
-
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({product}) => {
     const {img, name, price, description, category, stock} = product
+
+    const [quantity, setQuantity] = useState(0)
+
+    const handleOnAdd = (count) => {
+        setQuantity(count)
+    }
+
     return (
 
         <div className='polaroid asd'>
@@ -13,7 +22,8 @@ const ItemDetail = ({product}) => {
                 <h1 className='itemFuente asd'>Desc: {description}</h1>
                 <h1 className='itemFuente asd'>Categoria: {category}</h1>
                 <h1 className='itemFuente asd'>Disponible: {stock}</h1>
-            </div> 
+            </div>
+            { quantity > 0 ? <Link className='container button' to={'/cart'}>Finalizar compra</Link> : <ItemCount stock={stock} onConfirm={handleOnAdd}/>}
         </div>
 
     )
